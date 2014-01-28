0841bfeefc0ca93c31f1b1f96c1f4af93fc47f49
Estelleabn <estelleaubouin@gmail.com>
2014-01-28 12:00:20 +0100

    solution exo database 04

--- 04.CRUD/main.rb
+++ 04.CRUD/main.rb
@@ -6,35 +6,49 @@ db = SQLite3::Database.new(db_path)
 
 # creates the schema of the database
 # your code here
-sql = %q{
-  
+sql = %q{ CREATE TABLE IF NOT EXISTS Recipe(
+	 	id INTEGER primary key autoincrement,
+		name VARCHAR(255),
+	  	description VARCHAR(255),
+	  	length INTEGER,
+	  	difficulty INTEGER
+		)
 }
+
 db.execute(sql)
 
 #*************************************
 
 # function to create a recipe
 def create_recipe(db,name,description,length,difficulty)
-  # your code here
+  sql  = "INSERT INTO Recipe (name, description, length, difficulty) VALUES ('#{name}','#{description}',#{length},#{difficulty})"
+  p sql
+  db.execute(sql)
+  "Congrats! Your #{name} recipe was created successfully"
 end
 
 # function to delete a recipe
 def delete_recipe(db,id)
-  # your code here
+  db.execute("DELETE FROM Recipe WHERE name='#{id}'")
+  puts "Your #{id} recipe was deleted successfully"
 end
 
 # function to delete all recipes
 def delete_all_recipes(db)
-  # your code here
+  db.execute("DELETE FROM Recipe;")
 end
 
 # function to update a recipe
 def update_recipe(db,id,description)
-  # your code here
+  db.execute("UPDATE Recipe SET description='#{description}' WHERE name='#{id}'")
 end
 
 def get_recipes(db)
-  # function to get all recipes
+  puts db.execute("SELECT name FROM Recipe")
+end
+
+def read_recipes(db)
+  puts db.execute("SELECT name FROM Recipe")
 end
 
 
@@ -42,23 +56,50 @@ end
 
 # program
 
-puts "Salut Robuchon, what do you want to do today?"
-puts "1. create a recipe"
-puts "2. delete all recipes"
-puts "3. read your recipes"
 
-choice =  gets.chomp.to_i
 
-if choice == 1
-  # your code here to create a recipe
-  # you need to ask for name, description, length and difficulty
-  
-elsif choice == 2
-  # your code here to delete all recipes
-  
-elsif choice == 3
-  # your code here to read all recipes
-end 
+while true
+  puts "Salut Robuchon, what do you want to do today?"
+  puts "1. create a recipe"
+  puts "2. delete all recipes"
+  puts "3. delete only one recipe"
+  puts "4. read your recipes"
+  puts "5. update one recipe"
+  puts "6. exit"
+
+  choice =  gets.chomp.to_i
+
+  case choice
+    when 1
+      puts "Which recipe do you wanna create ?"
+      name = gets.chomp
+      puts "How do you cook it ?"
+      description = gets.chomp
+      puts "How long does it take to cook it ?"
+      length = gets.chomp.to_i
+      puts "Is it difficult to cook ? Select a number between 1 and 5."
+      difficulty = gets.chomp.to_i
+      create_recipe(db, name, description, length, difficulty)
+  when 2
+      delete_all_recipes(db)
+      puts "Your recipes list is now empty"
+  when 3
+      puts "Which recipe do you wanna delete ?"
+      id = gets.chomp
+      delete_recipe(db, id)
+  when 4
+  	puts "Here are all your recipes available :"
+  	read_recipes(db)
+  when 5
+  	puts "Which recipe do you wanna update"
+  	id = gets.chomp
+  	puts "Write your description"
+  	description = gets.chomp
+  	update_recipe(db, id, description)
+  when 6
+      break 
+  end
+end
   
   
   
