51378f17f243740b884564305cefc45d720e12bb
Estelleabn <estelleaubouin@gmail.com>
2014-01-28 15:46:55 +0100

    solution activerecord exo5

--- 05.CookBook/cookbook.rb
+++ 05.CookBook/cookbook.rb
@@ -11,11 +11,26 @@ puts "3. read your recipes"
 choice = gets.chomp.to_i
 
 if choice == 1
-  
+  puts "Which recipe do you wanna create ?"
+  name = gets.chomp
+  puts "How do you cook it ?"
+  description = gets.chomp
+  puts "How long does it take to cook it ?"
+  length = gets.chomp.to_i
+  puts "Is it difficult to cook ? Select a number between 1 and 5."
+  difficulty = gets.chomp.to_i
+  new_recipe = Recipe.create(name: name, description: description, length: length, difficulty: difficulty)
+
 elsif choice == 2
+  puts "Which recipe do you wanna delete ?"
+  id = gets.chomp
+  recipe_to_destroy = Recipe.find_by(name: id)
+  recipe_to_destroy.destroy
   
 elsif choice == 3
-  
+  recipes = Recipe.all
+  puts recipes
+
 else
   puts "i did not understand"
   
--- 05.CookBook/models/recipe.rb
+++ 05.CookBook/models/recipe.rb
@@ -1,2 +1,5 @@
-class Recipe < ActiveRecord::Base
+class Recipe < ActiveRecord::Base	
+	def to_s
+		"#{name} : \nA cuisiner avec : #{description} \nTake #{length} min to cook \nDifficulty : #{difficulty} / 5"
+	end
 end
